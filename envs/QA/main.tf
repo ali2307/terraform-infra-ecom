6d99bccebf7752180d04528fdc63753f75c2d534
Uthuman Ali <uthuman.a@fbma.org.ae>
2026-01-28 13:24:49 +0400

    update

--- envs/QA/main.tf
+++
@@ -1,5 +0,0 @@
-module "vpc" {
-  source = "../../modules/vpc"
-  cidr   = "10.20.0.0/16"
-  name   = "qa-vpc"
-}
--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -1,6 +1,82 @@
 resource "aws_vpc" "this" {
-  cidr_block = var.cidr
+  cidr_block = var.vpc_cidr
+
   tags = {
-    Name = var.name
+    Name        = "${var.project_name}-${var.environment}-vpc"
+    Environment = var.environment
   }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-igw"
+  }
+}
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+
+  vpc_id     = aws_vpc.this.id
+  cidr_block = element(var.public_subnets, count.index)
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-public-${count.index}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id     = aws_vpc.this.id
+  cidr_block = element(var.private_subnets, count.index)
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-private-${count.index}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-nat"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
